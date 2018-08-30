2e0ca7e54ec6a6cdb72311b34751956908ad26e1
pranav661 <37332556+pranav661@users.noreply.github.com>
2018-08-30 19:31:47 +0530

    Update replace.ps1

--- powershell/replace.ps1
+++ powershell/replace.ps1
@@ -1,1 +1,46 @@
-echo "Hello World"
+echo "Starting to replace the tokens in Web.config file"
+
+$configLoc = "$(Build.SourcesDirectory)\aspnet4-sample\Web.config"
+$config_with_valuesLoc = "$(Build.SourcesDirectory)\powershell\config_with_values.config"
+
+$tokens = (Get-Content $configLoc | select-string -pattern "{.*?}").length
+
+$lines = Get-Content $configLoc | select-string -pattern "{.*?}" | Out-String
+$lines = $lines.Trim()
+
+
+function replacing {
+Param($holder,$value)
+$original = Get-Content $configLoc
+$original | % { $_.Replace("$holder", "$value") } | Set-Content $configLoc
+}
+
+
+function getvalue {
+Param($prop,$holder)
+$value = Get-Content $config_with_valuesLoc | findstr $prop | Out-String | ForEach-Object { $_.Trim() }
+echo "$value"
+replacing -holder $holder -value $value
+}
+
+
+function getproperty {
+Param($holder)
+$property = $holder.split(" ")[1]
+$property = $property.split("=")[1]
+$property = $property.Trim()
+echo "$property"
+
+getvalue -prop $property -holder $holder
+}
+
+
+For ($i=0; $i -lt $tokens; $i++)
+{
+$line = ($lines -split '\r?\n')[$i]
+#echo "$line"
+getproperty -holder $line
+}
+
+
+
